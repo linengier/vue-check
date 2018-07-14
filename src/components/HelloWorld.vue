d414b37e499456dc0fddffcb46deec90957f4bea
mtboom <mtboom@163com>
2018-07-14 17:06:08 +0800

    修改外链标签

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <img src="../assets/logo.png">
     <h1><a href="https://cn.vuejs.org/">Vue</a> & <a href="https://www.iviewui.com/">iView</a></h1>
-    <h3><router-link :to='link'>{{title}}</router-link></h3>
+    <h3><a :link='link'>{{title}}</a></h3>
     <template v-for='(item,index) in list'>
       <template v-if="item.children">
         <h2>{{item.title}}</h2>
